7d4d9eab8d99517f579404d47af95bc4f7912493
Ben Gauslin <ben@gauslin.com>
2019-08-10 19:42:41 -0400

    TS updates for Tools

--- src/js/modules/Tools.ts
+++ src/js/modules/Tools.ts
@@ -1,15 +1,12 @@
 import fastclick from 'fastclick';
 
-/** @enum {string} */
-const NO_TOUCH_ATTR = 'no-touch';
+const NO_TOUCH_ATTR: string = 'no-touch';
 
-/** @class */
 class Tools {
   /**
    * Initializes all utility methods.
-   * @public
    */
-  init() {
+  public init(): void {
     this.noTouch_();
     this.viewportHeight();
     this.googleAnalytics_();
@@ -17,24 +14,23 @@ class Tools {
 
   /**
    * Initializes Google Analytics tracking.
-   * @private 
    */
-  googleAnalytics_() {
+  private googleAnalytics_(): void {
     if (process.env.NODE_ENV === 'production') {
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*<any>new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', process.env.GA_ID, 'auto');
+      (<any>window).ga('create', process.env.GA_ID, 'auto');
     }
   }
 
+  // TODO: Fix TS warning for fastclick.attach()
   /**
    * Adds 'no-touch' attribute if not a touch-enabled device.
-   * @private
    */
-  noTouch_() {
-    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+  private noTouch_(): void {
+    if ('ontouchstart' in window || (<any>window).DocumentTouch) {
       fastclick.attach(document.body);
     } else {
       document.body.setAttribute(NO_TOUCH_ATTR, '');
@@ -44,9 +40,8 @@ class Tools {
   /**
    * Sets custom property for viewport height that updates 'vh' calculation due
    * to iOS Safari behavior where chrome appears and disappears when scrolling.
-   * @public
    */
-  viewportHeight() {
+  public viewportHeight(): void {
     const viewportUnit = window.innerHeight / 100;
     document.documentElement.style.setProperty('--viewport-unit', `${viewportUnit}px`);
   }
