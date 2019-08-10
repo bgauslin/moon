407b12c447184be6f0b4c60ef4b533d7b0f41ff2
Ben Gauslin <ben@gauslin.com>
2019-08-10 19:42:06 -0400

    TS updates for Constants

--- src/js/modules/Constants.ts
+++ src/js/modules/Constants.ts
@@ -1,29 +1,27 @@
-/** @enum {string} */
-const Attribute = {
-  COLOR: 'color',
-  DIRECTION: 'direction',
-  ENABLED: 'enabled',
-  END: 'end',
-  HIDDEN: 'hidden',
-  ILLUMINATION: 'illumination',
-  LOADING: 'loading',
-  LOCATION: 'location',
-  PERCENT: 'percent',
-  PHASE: 'phase',
-  READY: 'ready',
-  RESTORE: 'restore',
-  START: 'start',
+enum Attribute {
+  COLOR = 'color',
+  DIRECTION = 'direction',
+  ENABLED = 'enabled',
+  END = 'end',
+  HIDDEN = 'hidden',
+  ILLUMINATION = 'illumination',
+  LOADING = 'loading',
+  LOCATION = 'location',
+  PERCENT = 'percent',
+  PHASE = 'phase',
+  READY = 'ready',
+  RESTORE = 'restore',
+  START = 'start',
 };
 
 /**
  * NOTE: Keep values coordinated with 'Chart' enum in 'src/stylus/config/layout.styl'
- * @enum {number}
  */
-const Chart = {
-  LABEL_GAP: 8,
-  MARGIN: 44,
-  SIZE: 320,
-  SWEEP_WIDTH: 72,
+enum Chart {
+  LABEL_GAP = 8,
+  MARGIN = 44,
+  SIZE = 320,
+  SWEEP_WIDTH = 72,
 };
 
 export { Attribute, Chart };
