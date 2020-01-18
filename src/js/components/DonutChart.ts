bc73382f25e03c55e997b4531e8a1f7c0cedda40
Ben Gauslin <ben@gauslin.com>
2020-01-18 07:56:55 -0500

    all constructor properties in DonutChart moved to method as const's since they're only needed within a single method (no-op)

--- src/js/components/DonutChart.ts
+++ src/js/components/DonutChart.ts
@@ -20,30 +20,8 @@ interface Point {
 const AXIS_OFFSET: number = -90;
 
 class DonutChart extends HTMLElement {
-  private circumference_: number;
-  private color_: string;
-  private cx_: number;
-  private cy_: number;
-  private end_: string;
-  private height_: number;
-  private radius_: number;
-  private radiusForLabels_: number;
-  private start_: string;
-  private width_: number;  
-  
   constructor() {
     super();
-
-    // [1] 2πr
-    // [2] Gap from edge of chart's arc for label placement.
-    this.color_ = this.getAttribute(Attribute.COLOR);
-    this.height_ = Chart.SIZE + (Chart.MARGIN * 2);
-    this.width_ = Chart.SIZE + (Chart.MARGIN * 2);
-    this.cx_ = this.height_ / 2;
-    this.cy_ = this.width_ / 2;
-    this.radius_ = (Chart.SIZE - Chart.SWEEP_WIDTH) / 2;
-    this.circumference_ = 2 * Math.PI * this.radius_; // [1]
-    this.radiusForLabels_ = (Chart.SIZE / 2) + Chart.LABEL_GAP; // [2]
   }
 
   static get observedAttributes(): string[] {
@@ -58,18 +36,28 @@ class DonutChart extends HTMLElement {
    * Renders an SVG circle with an arc for the sun/moon chart.
    */
   private render_(): void {
-    this.start_ = this.getAttribute(Attribute.START);
-    this.end_ = this.getAttribute(Attribute.END);
+    // Bail if both 'start' and 'end' attributes are missing since we can't
+    // make the chart without them.
+    const start = this.getAttribute(Attribute.START);
+    const end = this.getAttribute(Attribute.END);
 
-    if (!this.start_ || !this.end_) {
+    if (!start || !end) {
       return;
     }
 
+    // Set up the math for drawing arc sweeps.
+    const height: number = Chart.SIZE + (Chart.MARGIN * 2);
+    const width: number = Chart.SIZE + (Chart.MARGIN * 2);
+    const cx: number = height / 2;
+    const cy: number = width / 2;
+    const radius: number = (Chart.SIZE - Chart.SWEEP_WIDTH) / 2;
+    const circumference: number = 2 * Math.PI * radius; // [1]
+    
     // Convert values to degrees for arc. 'sweep' start and end values are
     // adjusted since the default start for SVG circles is 3 o'clock and we
     // want arcs to start at 6 o'clock, which is midnight graphically.
-    const sweepStart = this.timeToDegrees_(this.start_) + AXIS_OFFSET;
-    const sweepEnd = this.timeToDegrees_(this.end_) + AXIS_OFFSET;
+    const sweepStart = this.timeToDegrees_(start) + AXIS_OFFSET;
+    const sweepEnd = this.timeToDegrees_(end) + AXIS_OFFSET;
 
     // 'sweep' = arc length with any negative values converted to positive
     // for cleaner math.
@@ -79,52 +67,55 @@ class DonutChart extends HTMLElement {
     }
 
     // Calculate arc stroke values for SVG.
-    const strokeOffset = (sweep / 360) * this.circumference_;
-    const strokeDashOffset = this.circumference_ - strokeOffset;
+    const strokeOffset = (sweep / 360) * circumference;
+    const strokeDashOffset = circumference - strokeOffset;
 
     // Arc stroke rotation based on sweep's 'start' value
-    const transform = `rotate(${sweepStart}, ${this.cx_}, ${this.cy_})`;
+    const transform = `rotate(${sweepStart}, ${cx}, ${cy})`;
 
     // Label for 'rise' time.
-    const riseTime = this.start_;
+    const riseTime = start;
     const riseSweep = this.labelPlacement_(sweepStart);
     const riseRotation = this.labelRotation_({
       radius: riseSweep.radius,
       angle: sweepStart,
-      xOffset: this.cx_,
-      yOffset: this.cy_,
+      xOffset: cx,
+      yOffset: cy,
     });
     const riseTransform = `rotate(${riseSweep.sweep}, ${riseRotation.x}, ${riseRotation.y})`;
 
     // Label for 'set' time.
-    const setTime = this.end_;
+    const setTime = end;
     const setSweep = this.labelPlacement_(sweepEnd);
     const setRotation = this.labelRotation_({
       radius: setSweep.radius,
       angle: sweepEnd,
-      xOffset: this.cx_,
-      yOffset: this.cy_,
+      xOffset: cx,
+      yOffset: cy,
     });
     const setTransform = `rotate(${setSweep.sweep}, ${setRotation.x}, ${setRotation.y})`;
 
+    // Get color for the rendered arc.
+    const color = this.getAttribute(Attribute.COLOR);
+
     // Rendered chart HTML.
     const html = `
-      <svg viewbox="0 0 ${this.height_} ${this.width_}">\
+      <svg viewbox="0 0 ${height} ${width}">\
         <g>\
           <circle \
-            cx="${this.cx_}" \
-            cy="${this.cy_}" \
-            r="${this.radius_}" \
+            cx="${cx}" \
+            cy="${cy}" \
+            r="${radius}" \
             fill="transparent" \
-            stroke="${this.color_}" \
+            stroke="${color}" \
             stroke-width="${Chart.SWEEP_WIDTH}" \
-            stroke-dasharray="${this.circumference_}" \
+            stroke-dasharray="${circumference}" \
             stroke-dashoffset="${strokeDashOffset}" \
             transform="${transform}"></circle>\
-          <text class="chart__label" fill="${this.color_}" \
+          <text class="chart__label" fill="${color}" \
             x="${riseRotation.x}" y="${riseRotation.y}" \
             transform="${riseTransform}">${riseTime}</text>\
-          <text class="chart__label" fill="${this.color_}" \
+          <text class="chart__label" fill="${color}" \
             x="${setRotation.x}" y="${setRotation.y}" \
             transform="${setTransform}">${setTime}</text>\
         </g>\
@@ -140,17 +131,19 @@ class DonutChart extends HTMLElement {
    * side need a slight radius adjustment.
    */
   private labelPlacement_(degrees: number): Arc {
+    // Gap from edge of chart's arc for label placement.
+    let radiusForLabels = (Chart.SIZE / 2) + Chart.LABEL_GAP;
     let sweep = degrees;
-    let radius = this.radiusForLabels_;
+
     const leftSideStart = 180 + AXIS_OFFSET;
     const leftSideEnd = leftSideStart + 180;
 
     if (degrees > leftSideStart && degrees < leftSideEnd) {
       sweep -= 180;
-      radius += Chart.MARGIN - Chart.LABEL_GAP;
+      radiusForLabels += Chart.MARGIN - Chart.LABEL_GAP;
     }
 
-    return {radius, sweep};
+    return {radius: radiusForLabels, sweep};
   }
 
   /**
