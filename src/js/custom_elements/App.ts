4a3e948ef3926b8ac16461983ccbd747d0c9173c
Ben Gauslin <bgauslin@gmail.com>
2020-05-02 10:11:19 -0400

    element and property renaming in App (no-op)

--- src/js/custom_elements/App.ts
+++ src/js/custom_elements/App.ts
@@ -21,15 +21,15 @@ class App extends HTMLElement {
   private date_: AppDate;
   private dateTimeUtils_: DateTimeUtils;
   private location_: string;
-  private locationEl_: HTMLElement;
-  private locationObserver_: MutationObserver;
   private popstateListener_: any;
+  private userLocation_: HTMLElement;
+  private userLocationObserver_: MutationObserver;
   private utils_: Utils;
 
   constructor() {
     super();
     this.dateTimeUtils_ = new DateTimeUtils();
-    this.locationObserver_ = new MutationObserver(() => this.update_());
+    this.userLocationObserver_ = new MutationObserver(() => this.update_());
     this.utils_ = new Utils();
     this.popstateListener_ = this.update_.bind(this);
     this.addEventListener('click', this.handleClick_);
@@ -41,8 +41,8 @@ class App extends HTMLElement {
    */
   connectedCallback(): void {
     this.setupDom_();
-    this.locationEl_ = this.querySelector('.location');
-    this.locationObserver_.observe(this.locationEl_, {attributes: true});
+    this.userLocation_ = this.querySelector('user-location');
+    this.userLocationObserver_.observe(this.userLocation_, {attributes: true});
     this.utils_.init();
   }
 
@@ -68,7 +68,7 @@ class App extends HTMLElement {
 
     // Get date and location, then fetch data.
     this.date_ = this.dateTimeUtils_.activeDate();
-    this.location_ = this.locationEl_.getAttribute(LOCATION_ATTR);
+    this.location_ = this.userLocation_.getAttribute(LOCATION_ATTR);
     const moonData = await new DataFetcher().fetch(this.date_, this.location_);
     if (!moonData) {
       document.body.removeAttribute(LOADING_ATTR);
@@ -76,7 +76,7 @@ class App extends HTMLElement {
     }
 
     // Update the DOM and send a pageview.
-    this.updateHeader_();
+    this.updateCurrentDate_();
     this.updateElements_(moonData);
     this.updateDocumentTitle_({
       date: this.date_,
@@ -95,7 +95,7 @@ class App extends HTMLElement {
   /**
    * Updates an element with the current date in human-friendly format.
    */
-  private updateHeader_(): void {
+  private updateCurrentDate_(): void {
     const currentDateElement = this.querySelector('.header__link');
     currentDateElement.textContent = this.dateTimeUtils_.prettyDate(
       this.date_,
@@ -112,18 +112,18 @@ class App extends HTMLElement {
     const {hemisphere, illumination, moonrise, moonset, percent, phase, sunrise, sunset} = moonData;
 
     const items = [
-      ['.info', 'percent', String(percent)],
-      ['.info', 'phase', String(phase)],
-      ['.photo', 'hemisphere', hemisphere],
-      ['.photo', 'illumination', String(illumination)],
-      ['.photo', 'percent', String(percent)],
-      ['.photo', 'phase', phase],
-      ['[name=moon]', 'start', moonrise],
-      ['[name=moon]', 'end', moonset],
-      ['[name=sun]', 'start', sunrise],
-      ['[name=sun]', 'end', sunset],
-      ['[direction=next]', 'location', this.location_],
-      ['[direction=prev]', 'location', this.location_],
+      ['moon-info', 'percent', String(percent)],
+      ['moon-info', 'phase', String(phase)],
+      ['moon-photo', 'hemisphere', hemisphere],
+      ['moon-photo', 'illumination', String(illumination)],
+      ['moon-photo', 'percent', String(percent)],
+      ['moon-photo', 'phase', phase],
+      ['donut-chart[name=moon]', 'start', moonrise],
+      ['donut-chart[name=moon]', 'end', moonset],
+      ['donut-chart[name=sun]', 'start', sunrise],
+      ['donut-chart[name=sun]', 'end', sunset],
+      ['prev-next[direction=next]', 'location', this.location_],
+      ['prev-next[direction=prev]', 'location', this.location_],
       ['app-today', 'update', ''],
     ];
 
