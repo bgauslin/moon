c5318077b4000e2b20cd8e34b097601c17a3b475
Ben Gauslin <bgauslin@gmail.com>
2021-02-15 07:59:43 -0500

    chart axes math moved to constants

--- src/js/modules/Constants.ts
+++ src/js/modules/Constants.ts
@@ -5,3 +5,71 @@ export enum Chart {
   SIZE = 320,
   SWEEP_WIDTH = 72,
 }
+
+// Math for chart axes based on values in the Chart enum.
+interface Coordinates {
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+}
+
+// Container and centerpoint.
+export const VIEWBOX: number = Chart.SIZE + (Chart.MARGIN * 2);
+const MIDPOINT: number = VIEWBOX / 2;
+
+// Top and left axis lines.
+const AXIS_FIRST_HALF_START: number = Chart.MARGIN;
+const AXIS_FIRST_HALF_END: number = Chart.MARGIN + Chart.SWEEP_WIDTH;
+
+// Bottom and right axis lines.
+const AXIS_SECOND_HALF_START: number = MIDPOINT + ((Chart.SIZE / 2) - Chart.SWEEP_WIDTH)
+const AXIS_SECOND_HALF_END: number = VIEWBOX - Chart.MARGIN;
+
+// Axes values start at the top and go clockwise.
+export const AXES: Coordinates[] = [
+  {
+    x1: MIDPOINT, y1: AXIS_FIRST_HALF_START,
+    x2: MIDPOINT, y2: AXIS_FIRST_HALF_END,
+  },
+  {
+    x1: AXIS_SECOND_HALF_START, y1: MIDPOINT,
+    x2: AXIS_SECOND_HALF_END, y2: MIDPOINT,
+  },
+  {
+    x1: MIDPOINT, y1: AXIS_SECOND_HALF_START,
+    x2: MIDPOINT, y2: AXIS_SECOND_HALF_END,
+  },
+  {
+    x1: AXIS_FIRST_HALF_START, y1: MIDPOINT,
+    x2: AXIS_FIRST_HALF_END, y2: MIDPOINT,
+  },
+];
+
+// Top and left tick lines.
+const TICK_FIRST_HALF_START: number = 0;
+const TICK_FIRST_HALF_END: number = Chart.MARGIN;
+
+// Bottom and right tick lines.
+const TICK_SECOND_HALF_START: number = VIEWBOX - Chart.MARGIN;
+const TICK_SECOND_HALF_END: number = VIEWBOX;
+
+// Ticks values start at the top and go clockwise.
+export const TICKS: Coordinates[] = [
+  {
+    x1: MIDPOINT, y1: TICK_FIRST_HALF_START,
+    x2: MIDPOINT, y2: TICK_FIRST_HALF_END,
+  },
+  {
+    x1: TICK_SECOND_HALF_START, y1: MIDPOINT,
+    x2: TICK_SECOND_HALF_END, y2: MIDPOINT,
+  },
+  {
+    x1: MIDPOINT, y1: TICK_SECOND_HALF_START,
+    x2: MIDPOINT, y2: TICK_SECOND_HALF_END,
+  },
+  {
+    x1: TICK_FIRST_HALF_START, y1: MIDPOINT,
+    x2: TICK_FIRST_HALF_END, y2: MIDPOINT,
+  },
+];
