608a88c3d6b4ef66a5fdfa4877e9e3721710597a
Ben Gauslin <bgauslin@gmail.com>
2021-02-28 07:19:59 -0500

    single tick objects with single method to generate everything

--- src/js/custom_elements/moon_charts/moon_charts.ts
+++ src/js/custom_elements/moon_charts/moon_charts.ts
@@ -1,4 +1,7 @@
-import {Coordinates, MAJOR_TICKS, MINOR_TICK, SWEEP_TICKS, VIEWBOX} from '../../modules/Constants';
+import {CENTER, MAJOR_TICK, MINOR_TICK, SVGLine, SWEEP_TICK, VIEWBOX} from '../../modules/Constants';
+
+const DIVISIONS: number = 24;
+const ANGLE: number = 360 / DIVISIONS;
 
 export class MoonCharts extends HTMLElement {
   constructor() {
@@ -13,37 +16,44 @@ export class MoonCharts extends HTMLElement {
    * Renders an SVG containing grouped lines.
    */
   private setup() {
+    const sweepTicks = this.ticks(SWEEP_TICK, true);
+    const majorTicks = this.ticks(MAJOR_TICK, true);
+    const minorTicks = this.ticks(MINOR_TICK);
+
     const template = require('./moon_charts.pug');
     this.innerHTML += template({
       groups: [
-        {id: 'sweep-ticks', lines: SWEEP_TICKS},
-        {id: 'major-ticks', lines: MAJOR_TICKS},
-        {id: 'minor-ticks', lines: this.minorTicks()},
+        {id: 'sweep-ticks', lines: sweepTicks},
+        {id: 'major-ticks', lines: majorTicks},
+        {id: 'minor-ticks', lines: minorTicks},
       ],
       viewbox: VIEWBOX,
     });
   }
 
-  private minorTicks() {
-    const count = 24;
-    const rotation = 360 / count;
-    const {line, center} = MINOR_TICK;
+  /**
+   * Generates an array of coordinates for rendering SVG <line> elements.
+   * By default, a line for each hour is rendered unless the lines are 'major',
+   * which only renders a line every 90 degrees.
+   */
+  private ticks(tick: SVGLine, major: boolean = false) {
+    const {x1, y1, x2, y2} = tick;
     const ticks = [];
 
-    for (let i = 1; i <= count; i++) {
-      const degrees = i * rotation;
-      if (degrees % 90 !== 0) {
+    for (let i = 1; i <= DIVISIONS; i++) {
+      const degrees = i * ANGLE;
+      if (major && degrees % 90 === 0 ||
+          !major && degrees % 90 !== 0) {
         const newLine = {
-          x1: line.x1,
-          y1: line.y1,
-          x2: line.x2,
-          y2: line.y2,
-          transform: `rotate(${degrees}, ${center.x}, ${center.y})`,
+          x1,
+          y1,
+          x2,
+          y2,
+          transform: `rotate(${degrees}, ${CENTER}, ${CENTER})`,
         };
         ticks.push(newLine);
       }
     }
-
     return ticks;
   }
 }
