7a2bda1a96366e2df301b3ff5f0d52fd1a6f3a5e
Ben Gauslin <bgauslin@gmail.com>
2021-02-28 07:25:36 -0500

    simplifies and cleans up Constants (no-op)

--- src/js/modules/Constants.ts
+++ src/js/modules/Constants.ts
@@ -1,11 +1,3 @@
-// Values are coordinated with 'Chart' enum in 'src/stylus/config/constants.styl'
-export enum Chart {
-  LABEL_GAP = 8,
-  MARGIN = 44,
-  SIZE = 320,
-  SWEEP_WIDTH = 72,
-}
-
 export interface SVGLine {
   x1: number,
   y1: number,
@@ -13,23 +5,29 @@ export interface SVGLine {
   y2: number,
 }
 
-// Container and center point.
+// Values are coordinated with 'Chart' enum in 'src/stylus/config/constants.styl'
+export enum Chart {
+  LABEL_GAP = 8,
+  MARGIN = 44,
+  MINOR_TICK_LENGTH = 12,
+  SIZE = 320,
+  SWEEP_WIDTH = 72,
+}
+
+export const CENTER: number = Chart.MARGIN + (Chart.SIZE / 2);
 export const VIEWBOX: number = Chart.SIZE + (Chart.MARGIN * 2);
-export const CENTER: number = VIEWBOX / 2;
 
-export const SWEEP_TICK = {  
-  x1: CENTER, y1: Chart.MARGIN,
-  x2: CENTER, y2: Chart.MARGIN + Chart.SWEEP_WIDTH,
-};
-
-export const MAJOR_TICK = {
+export const MAJOR_TICK: SVGLine  = {
   x1: CENTER, y1: 0,
   x2: CENTER, y2: Chart.MARGIN,
 };
 
-export const MINOR_TICK = {
-  x1: CENTER,
-  y1: VIEWBOX - 12,
-  x2: CENTER,
-  y2: VIEWBOX,
+export const MINOR_TICK: SVGLine  = {
+  x1: CENTER, y1: VIEWBOX - Chart.MINOR_TICK_LENGTH,
+  x2: CENTER, y2: VIEWBOX,
+};
+
+export const SWEEP_TICK: SVGLine = {  
+  x1: CENTER, y1: Chart.MARGIN,
+  x2: CENTER, y2: Chart.MARGIN + Chart.SWEEP_WIDTH,
 };
