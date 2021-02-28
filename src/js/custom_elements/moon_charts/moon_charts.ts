7b423231dfe374afdd9b5444b6070aa96aa27fc8
Ben Gauslin <bgauslin@gmail.com>
2021-02-28 06:23:15 -0500

    minor ticks wired up and working, ready for tweaking

--- src/js/custom_elements/moon_charts/moon_charts.ts
+++ src/js/custom_elements/moon_charts/moon_charts.ts
@@ -1,4 +1,4 @@
-import {MAJOR_TICKS, SWEEP_TICKS, VIEWBOX} from '../../modules/Constants';
+import {Coordinates, MAJOR_TICKS, MINOR_TICK, SWEEP_TICKS, VIEWBOX} from '../../modules/Constants';
 
 export class MoonCharts extends HTMLElement {
   constructor() {
@@ -18,9 +18,33 @@ export class MoonCharts extends HTMLElement {
       groups: [
         {id: 'sweep-ticks', lines: SWEEP_TICKS},
         {id: 'major-ticks', lines: MAJOR_TICKS},
+        {id: 'minor-ticks', lines: this.minorTicks()},
       ],
       viewbox: VIEWBOX,
     });
+  }
+
+  private minorTicks() {
+    const count = 24;
+    const rotation = 360 / count;
+    const {line, center} = MINOR_TICK;
+    const ticks = [];
+
+    for (let i = 1; i <= count; i++) {
+      const degrees = i * rotation;
+      if (degrees % 90 !== 0) {
+        const newLine = {
+          x1: line.x1,
+          y1: line.y1,
+          x2: line.x2,
+          y2: line.y2,
+          transform: `rotate(${degrees}, ${center.x}, ${center.y})`,
+        };
+        ticks.push(newLine);
+      }
+    }
+
+    return ticks;
   }
 }
 
--- src/js/modules/Constants.ts
+++ src/js/modules/Constants.ts
@@ -7,54 +7,70 @@ export enum Chart {
 }
 
 // Math for chart axes based on values in the Chart enum.
-interface Coordinates {
+export interface Coordinates {
   x1: number,
   y1: number,
   x2: number,
   y2: number,
+  rotate?: number,
 }
 
 // Container and center point.
 export const VIEWBOX: number = Chart.SIZE + (Chart.MARGIN * 2);
-const MIDPOINT: number = VIEWBOX / 2;
+const CENTER: number = VIEWBOX / 2;
 
 // Four overlay 90 degree ticks in the moon/set sweep area.
-const SWEEP_INNER_EDGE: number = MIDPOINT + ((Chart.SIZE / 2) - Chart.SWEEP_WIDTH);
+const SWEEP_INNER_EDGE: number = CENTER + ((Chart.SIZE / 2) - Chart.SWEEP_WIDTH);
 export const SWEEP_TICKS: Coordinates[] = [
   { // top
-    x1: MIDPOINT, y1: Chart.MARGIN,
-    x2: MIDPOINT, y2: Chart.MARGIN + Chart.SWEEP_WIDTH,
+    x1: CENTER, y1: Chart.MARGIN,
+    x2: CENTER, y2: Chart.MARGIN + Chart.SWEEP_WIDTH,
   },
   { // right
-    x1: SWEEP_INNER_EDGE, y1: MIDPOINT,
-    x2: VIEWBOX - Chart.MARGIN, y2: MIDPOINT,
+    x1: SWEEP_INNER_EDGE, y1: CENTER,
+    x2: VIEWBOX - Chart.MARGIN, y2: CENTER,
   },
   { // bottom
-    x1: MIDPOINT, y1: SWEEP_INNER_EDGE,
-    x2: MIDPOINT, y2: VIEWBOX - Chart.MARGIN,
+    x1: CENTER, y1: SWEEP_INNER_EDGE,
+    x2: CENTER, y2: VIEWBOX - Chart.MARGIN,
   },
   { // left
-    x1: Chart.MARGIN, y1: MIDPOINT,
-    x2: Chart.MARGIN + Chart.SWEEP_WIDTH, y2: MIDPOINT,
+    x1: Chart.MARGIN, y1: CENTER,
+    x2: Chart.MARGIN + Chart.SWEEP_WIDTH, y2: CENTER,
   },
 ];
 
 // Four major 90 degree ticks outside the sweep area.
 export const MAJOR_TICKS: Coordinates[] = [
   { // top
-    x1: MIDPOINT, y1: 0,
-    x2: MIDPOINT, y2: Chart.MARGIN,
+    x1: CENTER, y1: 0,
+    x2: CENTER, y2: Chart.MARGIN,
   },
   { // right
-    x1: VIEWBOX - Chart.MARGIN, y1: MIDPOINT,
-    x2: VIEWBOX, y2: MIDPOINT,
+    x1: VIEWBOX - Chart.MARGIN, y1: CENTER,
+    x2: VIEWBOX, y2: CENTER,
   },
   { // bottom
-    x1: MIDPOINT, y1: VIEWBOX - Chart.MARGIN,
-    x2: MIDPOINT, y2: VIEWBOX,
+    x1: CENTER, y1: VIEWBOX - Chart.MARGIN,
+    x2: CENTER, y2: VIEWBOX,
   },
   { // left
-    x1: 0, y1: MIDPOINT,
-    x2: Chart.MARGIN, y2: MIDPOINT,
+    x1: 0, y1: CENTER,
+    x2: Chart.MARGIN, y2: CENTER,
   },
 ];
+
+// Single minor tick for generating multiple ticks.
+const MINOR_TICK_LENGTH = 12;
+export const MINOR_TICK = {
+  center: {
+    x: CENTER,
+    y: CENTER,
+  },
+  line: {
+    x1: CENTER,
+    y1: VIEWBOX - MINOR_TICK_LENGTH,
+    x2: CENTER,
+    y2: VIEWBOX,
+  },
+};
