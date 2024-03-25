4a291ccbf6f22bb229f40766e45254d04f676fbe
Ben Gauslin <github@gauslin.com>
2024-03-25 18:58:28 -0500

    simplifies info.ts (no-op)

--- src/js/custom_elements/info/info.ts
+++ src/js/custom_elements/info/info.ts
@@ -1,24 +1,20 @@
-const PERCENT_ATTR = 'percent';
-const PHASE_ATTR = 'phase';
-
 /**
  * Custom element that renders text for the moon phase and its percentage
  * relative to a full moon phase cycle.
  */
 export class MoonInfo extends HTMLElement {
-  private template: any;
 
   constructor() {
     super();
   }
 
   static get observedAttributes(): string[] {
-    return [PERCENT_ATTR, PHASE_ATTR];
+    return ['percent', 'phase'];
   }
 
   attributeChangedCallback() {
-    const percent = this.getAttribute(PERCENT_ATTR);
-    const phase = this.getAttribute(PHASE_ATTR);
+    const percent = this.getAttribute('percent');
+    const phase = this.getAttribute('phase');
     if (percent && phase) {
       const visibility = (percent === '0') ? 'visibility="invisible"' : '';
       this.innerHTML = `
