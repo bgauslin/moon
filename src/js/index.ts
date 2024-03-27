5d7ca62227ee05d7589e3ac390a760168c153dfe
Ben Gauslin <github@gauslin.com>
2024-03-26 19:21:32 -0500

    removes workbox and pwa
    TODO: write service worker from scratch instead of webpack plugin

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,3 @@
-// require ('dotenv').config();
-
 import './custom_elements/app/app';
 import './custom_elements/donut_chart/donut_chart';
 import './custom_elements/photo/photo';
@@ -12,19 +10,3 @@ import '../styles/index.scss';
 // Clean up the DOM since JS is enabled.
 document.body.classList.remove('no-js');
 document.body.querySelector('noscript')!.remove();
-
-// Register the Service Worker.
-if (process.env.NODE_ENV === 'production') {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js');
-    });
-  }
-}
-
-// Redirect view to '/' if app is launched as a standalone app. Otherwise,
-// a user may have saved the app with a full URL, which means they will start
-// at that URL every time they launch the app instead of on the current day.
-if ((window as any).navigator.standalone == true || window.matchMedia('(display-mode: standalone)').matches) {
-  history.replaceState(null, '', '/');
-}
