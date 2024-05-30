63d824f20a5fe03c4b4adc9016a92209ca982a34
Ben Gauslin <github@gauslin.com>
2024-05-30 11:40:38 -0500

    return of geolocation started

--- src/js/custom_elements/user_location/user_location.ts
+++ src/js/custom_elements/user_location/user_location.ts
@@ -16,6 +16,7 @@ class UserLocation extends HTMLElement {
   private dateUtils: DateUtils;
   private defaultLocation: string;
   private form: HTMLFormElement;
+  private geoButton: HTMLButtonElement;
   private hasSetup: boolean;
   private input: HTMLInputElement;
   private location: string;
@@ -81,6 +82,9 @@ class UserLocation extends HTMLElement {
         this.restore();
       }
     });
+
+    // Get user's location when geolocation button is clicked.
+    this.geoButton.addEventListener('click', () => this.getGeolocation());
   }
 
   /**
@@ -127,6 +131,60 @@ class UserLocation extends HTMLElement {
   }
 
   /**
+   * Fetches human-friendly location based on geo coordinates provided by the
+   * Geolocation API.
+   */
+  private async getGeolocation(): Promise<any> {
+    this.input.value = 'Getting location...';
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+  
+    // Alert user and restore input with previous location.
+    const error = () => {
+      alert(`Uh oh. We were unable to retrieve your location. :(\n\nYou may need to enable Location Services on your device before you can use this feature.`);
+      this.restore();
+    }
+
+    // Get user's location as city/state/country.
+    const success = (position: any) => {
+      this.reverseGeocode({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    }
+
+    // Get user's current geolocation coordinates.
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
+  /**
+   * Fetches human-friendly location based on geo coordinates via API.
+   */
+  private async reverseGeocode(coords: UserCoordinates): Promise<any> {
+    const {lat, lng} = coords;
+    const endpoint = `${process.env.GEOCODE_API}reverse?lat=${lat}&lon=${lng}&api_key=${process.env.GEOCODE_API_KEY}`;
+
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      const {address} = data;
+      const {city} = address;
+
+      this.location = city;
+
+      this.previousLocation = this.location;
+      this.input.value = this.location;
+      this.update();
+    } catch (error) {
+      console.warn('Currently unable to fetch data. :(');
+    }
+  }
+
+  /**
    * Renders HTML form element for user's location and sets references to
    * form elements.
    */
@@ -146,11 +204,26 @@ class UserLocation extends HTMLElement {
             <path d="M10,10 L14,14 M10,14 L14,10"/>
           </svg>
         </button>
+        ${this.renderGeoButton()}
       </form>
     `;
 
     this.form = <HTMLFormElement>this.querySelector('form');
     this.input = <HTMLInputElement>this.querySelector('input');
+    this.geoButton = <HTMLButtonElement>this.querySelector('[id="geo"]');
+  }
+
+  private renderGeoButton() {
+    if (navigator.geolocation) {
+      return `
+        <button type="button" aria-label="Get location" id="geo">
+          <svg viewBox="0 0 24 24" aria-hidden="true">
+            <path d="M12,2 v3 M22,12 h-3 M12,22 v-3 M2,12 h3 M5,12 A7 7 0 1 1 19,12 M5,12 A7 7 0 0 0 19,12"/>
+            <circle cx="12" cy="12" r="2.5"/>
+          </svg>
+        </button>
+      `;
+    }
   }
 }
 
