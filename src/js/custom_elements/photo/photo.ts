c30e32f3a0c540018cd0fdc0194e59e5900fdadb
Ben Gauslin <github@gauslin.com>
2024-06-01 20:05:24 -0500

    Refactors photo.ts with a spinner

--- src/js/custom_elements/photo/photo.ts
+++ src/js/custom_elements/photo/photo.ts
@@ -12,32 +12,48 @@ class MoonPhoto extends HTMLElement {
     return ['percent'];
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (newValue !== oldValue) {
+      this.updateImage(newValue);
+    }
+  }
+
   /**
    * Converts a percentage to its corresponding integer within a range of
    * numbers whose maximum is less than 100, then renders that image.
    */
-  attributeChangedCallback() {
-    const percent = Number(this.getAttribute('percent')); 
-    const currentFrame = Math.round((percent / 100) * IMAGE_COUNT);
+  updateImage(percent) {
+    const currentFrame = Math.round((Number(percent) / 100) * IMAGE_COUNT);
     const frame = (currentFrame === 0) ? IMAGE_COUNT : currentFrame;
     const index = (frame < 10) ? `0${frame}` : frame;
 
-    const image = document.createElement('img');
     const image1x = `${IMAGE_PATH}phase-${index}@1x.webp`;
     const image2x = `${IMAGE_PATH}phase-${index}@2x.webp`;
-    
-    image.alt = '';
-    image.src = image1x;
-    image.srcset = `${image1x} 1x, ${image2x} 2x`;
-    image.width = 240;
-    image.height = 240;
 
-    this.replaceChildren(image);
+    if (this.image) {
+      this.removeChild(this.image);
+    }
+
+    this.image = document.createElement('img');
+    this.image.alt = '';
+    this.image.src = image1x;
+    this.image.srcset = `${image1x} 1x, ${image2x} 2x`;
+    this.image.width = 240;
+    this.image.height = 240;
+
+    this.appendChild(this.image);
 
     window.requestAnimationFrame(() => {
-      if (!image.complete) {
-        image.dataset.loading = '';
-        image.onload = () => delete image.dataset.loading;
+      if (!this.image.complete) {
+        const spinner = document.createElement('div');
+        spinner.classList.add('spinner');
+        this.appendChild(spinner);
+
+        this.image.dataset.loading = '';
+        this.image.onload = () => {
+          delete this.image.dataset.loading;
+          this.image.addEventListener('transitionend', () => this.removeChild(spinner));
+        }
       }
     });
   }
