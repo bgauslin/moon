2fa917b591caeff33c54df9a0a36c2e869c7b36e
Ben Gauslin <github@gauslin.com>
2025-04-07 09:01:07 +0200

    renames folder (no-op)

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,7 @@
-import './custom_elements/app/app';
-import './custom_elements/chart/chart';
-import './custom_elements/location/location';
-import './custom_elements/photo/photo';
-import './custom_elements/ticks/ticks';
+import './components/app/app';
+import './components/chart/chart';
+import './components/location/location';
+import './components/photo/photo';
+import './components/ticks/ticks';
 
 import '../styles/index.scss';
