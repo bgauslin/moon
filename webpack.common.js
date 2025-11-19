74b99ccb3625d4a9bb4df175d8cc195195f87942
Ben Gauslin <github@gauslin.com>
2025-11-19 10:27:59 -0300

    prefetch webfonts instead of preload

--- webpack.common.js
+++ webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
     new FontPreloadPlugin({
       index: 'index.html',
       insertBefore: 'link:first-of-type',
-      loadType: 'preload',
+      loadType: 'prefetch',
     }),
   ],
   module: {
